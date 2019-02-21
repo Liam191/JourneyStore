7472da408338f8458c9ab1541c53fea6a0b0357d
Liam191 <liam.h.-@hotmail.com>
2019-02-21 06:02:13 +0000

    First commit

---
+++ app/src/main/java/com/jupiter191/journeystore/feature_journeylist/JourneyListActivity.java
@@ -0,0 +1,15 @@
+package com.jupiter191.journeystore.feature_journeylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jupiter191.journeystore.R;
+
+public class JourneyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journey_list);
+    }
+}
