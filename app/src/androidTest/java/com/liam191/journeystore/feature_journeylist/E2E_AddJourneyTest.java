d15d3b6b58119cf4e086d61df51c02fc9eb494c6
Liam191 <liam.h.-@hotmail.com>
2019-02-28 14:08:06 +0000

    Renamed add journey e2e test. Added journeylist e2e test.

---
+++ app/src/androidTest/java/com/liam191/journeystore/feature_journeylist/E2E_AddJourneyTest.java
@@ -0,0 +1,58 @@
+package com.liam191.journeystore.feature_journeylist;
+
+import com.liam191.journeystore.R;
+import com.liam191.journeystore.repo.Journey;
+import com.liam191.journeystore.repo.JourneyRepository;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class E2E_AddJourneyTest {
+
+
+    private static final int ADD_JOURNEY_BUTTON_ID = R.id.add_journey_btn;
+//    private static final int ADD_JOURNEY_DEPARTURE_TEXT_FIELD_ID = R.id.add_journey_departure_txt;
+//    private static final int ADD_JOURNEY_DESTINATION_TEXT_FIELD_ID = R.id.add_journey_destination_txt;
+//    private static final int ADD_JOURNEY_SUBMIT_BUTTON_ID = R.id.add_journey_submit_btn;
+
+    private final String departureLocationToBeTyped = "123 Mock St, Departureville, Dublin, Ireland";
+    private final String destinationToBeTyped = "456 Mock Road, Destination Town, Dublin, Ireland";
+
+    private JourneyRepository journeyRepository;
+
+    @Rule
+    public ActivityTestRule<JourneyListActivity> journeyListActivity = new ActivityTestRule(JourneyListActivity.class);
+
+    @Before
+    public void setup(){
+        journeyRepository = DaggerFakeJourneyRepositoryComponent
+                .builder()
+                .build()
+                .getJourneyRepository();
+    }
+
+    @Test
+    public void addNewJourney(){
+        onView(withId(ADD_JOURNEY_BUTTON_ID)).perform(click());
+//
+//        onView(withId(ADD_JOURNEY_DEPARTURE_TEXT_FIELD_ID)).perform(typeText(departureLocationToBeTyped));
+//        onView(withId(ADD_JOURNEY_DESTINATION_TEXT_FIELD_ID)).perform((typeText(destinationToBeTyped)));
+//
+//        onView(withId(ADD_JOURNEY_SUBMIT_BUTTON_ID)).perform(click());
+        assertThat(journeyRepository.getJourneys(), hasItem(new Journey(1)));
+
+    }
+}
---
+++ app/src/androidTest/java/com/liam191/journeystore/feature_journeylist/E2E_JourneyList.java
@@ -0,0 +1,102 @@
+package com.liam191.journeystore.feature_journeylist;
+
+import com.liam191.journeystore.R;
+import com.liam191.journeystore.feature_journeydetails.JourneyDetailsActivity;
+import com.liam191.journeystore.repo.DaggerJourneyRepositoryComponent;
+import com.liam191.journeystore.repo.Journey;
+import com.liam191.journeystore.repo.JourneyRepository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.test.espresso.intent.matcher.BundleMatchers;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasEntry;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class E2E_JourneyList {
+
+    private final String departureLocationToBeTyped = "123 Mock St, Departureville, Dublin, Ireland";
+    private final String destinationToBeTyped = "456 Mock Road, Destination Town, Dublin, Ireland";
+
+    private JourneyRepository journeyRepository;
+
+    @Rule
+    public ActivityTestRule<JourneyListActivity> journeyListActivity = new ActivityTestRule(JourneyListActivity.class);
+
+    @Rule
+    public ActivityTestRule<JourneyDetailsActivity> journeyDetailsActivity = new ActivityTestRule(JourneyDetailsActivity.class);
+
+    @Before
+    public void setup(){
+        // Using real repository with real underlying database for E2E tests.
+        journeyRepository = DaggerJourneyRepositoryComponent
+                .builder()
+                .build()
+                .getJourneyRepository();
+
+        clearJourneyLocalDatabase();
+    }
+
+    private void clearJourneyLocalDatabase() {
+        LiveData<List<Journey>> journeyList = journeyRepository.getJourneys();
+        for(Journey journey : journeyList.getValue()){
+//            journeyRepository.delete(journey.id());
+        }
+    }
+
+
+    @Test
+    public void addNewJourney(){
+        // JourneyListActivity
+        onView(withId(R.id.add_journey_btn)).perform(click());
+
+        // AddJourneyActivity
+//        onView(withId(R.id.add_journey_departure_txt)).perform(typeText(departureLocationToBeTyped));
+//        onView(withId(R.id.add_journey_destination_txt)).perform((typeText(destinationToBeTyped)));
+//        // Add journey with above details.
+//        onView(withId(R.id.add_journey_submit_btn)).perform(click());
+
+        // JourneyListActivity
+        // Check that the added journey is present in the journey list.
+        onData(allOf(
+                hasEntry(equalTo("departure"), is(departureLocationToBeTyped)),
+                hasEntry(equalTo("destination"), is(destinationToBeTyped))
+            )).check(matches(anything()));
+    }
+
+    @Test
+    public void viewJourneyDetails(){
+        // JourneyListActivity
+        onData(allOf(
+                hasEntry(equalTo("departure"), is(departureLocationToBeTyped)),
+                hasEntry(equalTo("destination"), is(destinationToBeTyped))
+        )).perform(click());
+
+        // JourneyDetailsActivity
+//        onView(withId(R.id.journey_departure_txt)).check(matches(withText(departureLocationToBeTyped)));
+//        onView(withId(R.id.journey_destination_txt)).check(matches(withText(destinationToBeTyped)));
+    }
+
+
+    @After
+    public void teardown(){
+        clearJourneyLocalDatabase();
+    }
+}
