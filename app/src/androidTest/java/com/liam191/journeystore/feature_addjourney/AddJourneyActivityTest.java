89156938b7ec279bb40eef40689040dbf8a2a4e3
Liam191 <liam.h.-@hotmail.com>
2019-03-07 21:43:52 +0000

    Commented out unfinished tests. Added tests for nextButton

--- app/src/androidTest/java/com/liam191/journeystore/feature_addjourney/AddJourneyActivityTest.java
+++ app/src/androidTest/java/com/liam191/journeystore/feature_addjourney/AddJourneyActivityTest.java
@@ -1,6 +1,10 @@
 package com.liam191.journeystore.feature_addjourney;
 
+import android.util.Log;
+
 import com.liam191.journeystore.R;
+import com.liam191.journeystore.repo.DaggerFakeJourneyRepositoryComponent;
+import com.liam191.journeystore.repo.JourneyRepositoryComponent;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,44 +16,70 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(AndroidJUnit4.class)
 @MediumTest
 public class AddJourneyActivityTest {
 
+    private static final String TAG = AddJourneyActivityTest.class.getSimpleName();
+    private final String departureLocationToBeTyped = "123 Mock St, Departureville, Dublin, Ireland";
+    private final String destinationToBeTyped = "456 Mock Road, Destination Town, Dublin, Ireland";
+
     @Rule
     public ActivityTestRule<AddJourneyActivity> addJourneyActivity = new ActivityTestRule<>(AddJourneyActivity.class);
+    private JourneyRepositoryComponent fakeJourneyRepositoryComponent;
 
     @Before
     public void setup(){
-
+        fakeJourneyRepositoryComponent = DaggerFakeJourneyRepositoryComponent
+                .builder()
+                .build();
     }
 
     @Test
     public void departureAddress_shouldBeDisplayed(){
-        onView(withId(R.id.departure_addr_txt))
+        onView(withId(R.id.addjourney_departure_addr_txt))
+                .check(matches(isDisplayed()));
+    }
+
+//    @Test
+//    public void destinationAddress_shouldBeDisplayed(){
+//        onView(withId(R.id.destination_addr_txt))
+//                .check(matches(isDisplayed()));
+//    }
+
+//    @Test
+//    public void journeyDuration_shouldBeDisplayed(){
+//        onView(withId(R.id.duration_num))
+//                .check(matches(isDisplayed()));
+//    }
+
+//    @Test
+//    public void journeyStartTime_shouldBeDisplayed(){
+//        onView(withId(R.id.start_time))
+//                .check(matches(isDisplayed()));
+//    }
+
+    @Test
+    public void nextButton_shouldBeDisplayed(){
+        onView(withId(R.id.addjourney_next_btn))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void destinationAddress_shouldBeDisplayed(){
-        onView(withId(R.id.destination_addr_txt))
-                .check(matches(isDisplayed()));
-    }
+    public void nextButton_givenNoJourneyDetails_onClick_shouldDisplayErrors(){
+        onView(withId(R.id.addjourney_departure_addr_txt))
+                .perform(typeText(departureLocationToBeTyped));
 
-    @Test
-    public void journeyDuration_shouldBeDisplayed(){
-        onView(withId(R.id.duration_num))
-                .check(matches(isDisplayed()));
-    }
+        onView(withId(R.id.addjourney_next_btn))
+                .perform(click());
 
-    @Test
-    public void journeyStartTime_shouldBeDisplayed(){
-        onView(withId(R.id.start_time))
-                .check(matches(isDisplayed()));
+        Log.e(TAG, fakeJourneyRepositoryComponent.getJourneyRepository().getJourneys().getValue().toString());
+//        assertThat(fakeJourneyRepositoryComponent.getJourneyRepository().getJourneys().getValue(),
+//                hasItem());
     }
-
 }
